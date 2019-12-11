5ef83428ccb7c453e260a56dcb8389c591f42187
riocha <48554742+riocha@users.noreply.github.com>
2019-12-11 21:34:25 +0900

    서범수_상담페이지수정_191211

---
+++ src/hagong/academy/mngStudent/mngCouns/controller/CounsSrchServlet.java
@@ -0,0 +1,47 @@
+package hagong.academy.mngStudent.mngCouns.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CounsSrchServlet
+ */
+@WebServlet("/asearch.couns")
+public class CounsSrchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CounsSrchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//검색관련 값 받아오기
+		String searchCondition = request.getParameter("searchCondition");
+		String srchCnt = request.getParameter("searchCnt");
+		
+		System.out.println("searchCondition : " + searchCondition);
+		System.out.println("srchCnt : " + srchCnt);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
