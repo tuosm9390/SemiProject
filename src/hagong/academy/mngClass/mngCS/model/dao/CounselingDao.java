cb725a60836c125ec5bbe51ed9a660079650f731
riocha <48554742+riocha@users.noreply.github.com>
2019-12-11 12:37:26 +0900

    서범수_191211_상담페이지작업중

--- src/hagong/academy/mngClass/mngCS/model/dao/CounselingDao.java
+++ src/hagong/academy/mngClass/mngCS/model/dao/CounselingDao.java
@@ -4,11 +4,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
 
 import hagong.academy.mngClass.mngCS.model.vo.MemberCouns;
+
+import static hagong.common.JDBCTemplate.*;
 
 public class CounselingDao {
 	Properties prop = new Properties();
@@ -30,6 +33,30 @@ public class CounselingDao {
 		ArrayList<MemberCouns> counsList = null;
 		
 		String query = prop.getProperty("selectCounsList");
+		
+		try {
+			stmt = con.createStatement();
+			
+			rset = stmt.executeQuery(query);
+			
+			counsList = new ArrayList<MemberCouns>();
+			
+			while(rset.next()) {
+				MemberCouns mc = new MemberCouns();
+				
+				mc.setUserNo(rset.getInt("USER_NO"));
+				
+				counsList.add(mc);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close(stmt);
+			close(rset);
+		}
+		
 		
 		return counsList;
 	}
--- src/hagong/academy/mngClass/mngCS/model/vo/Counseling.java
+++ src/hagong/academy/mngClass/mngCS/model/vo/Counseling.java
@@ -1,14 +1,15 @@
 package hagong.academy.mngClass.mngCS.model.vo;
 
+
 import java.sql.Date;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
 
-@Data
+/*@Data
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor*/
 public class Counseling {
 	private int couNo;
 	private String couTitle;
@@ -18,4 +19,93 @@ public class Counseling {
 	private String couContent;
 	private String couAction;
 	private String couBlack;
+	
+	public Counseling() {}
+	
+	public Counseling(int couNo, String couTitle, Date couDate, int couUserNo, String couType, String couContent,
+			String couAction, String couBlack) {
+		super();
+		this.couNo = couNo;
+		this.couTitle = couTitle;
+		this.couDate = couDate;
+		this.couUserNo = couUserNo;
+		this.couType = couType;
+		this.couContent = couContent;
+		this.couAction = couAction;
+		this.couBlack = couBlack;
+	}
+
+	public int getCouNo() {
+		return couNo;
+	}
+
+	public void setCouNo(int couNo) {
+		this.couNo = couNo;
+	}
+
+	public String getCouTitle() {
+		return couTitle;
+	}
+
+	public void setCouTitle(String couTitle) {
+		this.couTitle = couTitle;
+	}
+
+	public Date getCouDate() {
+		return couDate;
+	}
+
+	public void setCouDate(Date couDate) {
+		this.couDate = couDate;
+	}
+
+	public int getCouUserNo() {
+		return couUserNo;
+	}
+
+	public void setCouUserNo(int couUserNo) {
+		this.couUserNo = couUserNo;
+	}
+
+	public String getCouType() {
+		return couType;
+	}
+
+	public void setCouType(String couType) {
+		this.couType = couType;
+	}
+
+	public String getCouContent() {
+		return couContent;
+	}
+
+	public void setCouContent(String couContent) {
+		this.couContent = couContent;
+	}
+
+	public String getCouAction() {
+		return couAction;
+	}
+
+	public void setCouAction(String couAction) {
+		this.couAction = couAction;
+	}
+
+	public String getCouBlack() {
+		return couBlack;
+	}
+
+	public void setCouBlack(String couBlack) {
+		this.couBlack = couBlack;
+	}
+
+	@Override
+	public String toString() {
+		return "Counseling [couNo=" + couNo + ", couTitle=" + couTitle + ", couDate=" + couDate + ", couUserNo="
+				+ couUserNo + ", couType=" + couType + ", couContent=" + couContent + ", couAction=" + couAction
+				+ ", couBlack=" + couBlack + "]";
+	}
+	
+	
+	
 }
