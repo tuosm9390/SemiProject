310b2d7d62379997736c90a308da81a3d776da6e
user <user@DESKTOP-M8230D6>
2019-12-18 11:15:59 +0900

    드디어 수납목록은 받아왔서용! 이제 수정만 되면 돼용! ^^! 할 수 있게쬬!?!?!?! - 191218

--- src/hagong/academy/mngStudent/mngPurchase/model/dao/PurchaseDao.java
+++ src/hagong/academy/mngStudent/mngPurchase/model/dao/PurchaseDao.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 import hagong.academy.mngStudent.mngPurchase.model.vo.Purchase;
+import hagong.academy.mngStudent.mngPurchase.model.vo.SelectClass;
 import hagong.academy.mngStudent.mngPurchase.model.vo.SelectDate;
 
 import static hagong.common.JDBCTemplate.*;
@@ -48,6 +49,35 @@ public class PurchaseDao {
 		}
 		return startDay;
 	}
+	
+	public ArrayList<SelectClass> selectClass(Connection con, SelectDate selectDate) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ArrayList<SelectClass> selectClass = null;
+		String selectedDate = selectDate.getSelectYear() + "-" + selectDate.getSelectMonth();
+		
+		String query = prop.getProperty("selectClass");
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, selectedDate);
+			pstmt.setString(2, selectedDate);
+			rset = pstmt.executeQuery();
+			selectClass = new ArrayList<SelectClass>();
+			
+			while(rset.next()) {
+				SelectClass sc = new SelectClass();
+				sc.setClassNo(rset.getInt("CLS_NO"));
+				sc.setClassName(rset.getString("CLS_NAME"));
+				selectClass.add(sc);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return selectClass;
+	}
 
 	public ArrayList<Purchase> selectPurchaseList(Connection con, SelectDate selectDate) {
 		PreparedStatement pstmt = null;
@@ -77,13 +107,72 @@ public class PurchaseDao {
 				purchase.setClassStart(rset.getDate("CLS_START"));
 				purchase.setClassEnd(rset.getDate("CLS_END"));
 				purchase.setStuId(rset.getString("USER_ID"));
-				purchase.setBeneName(rset.getString("NAME"));
+				purchase.setStuName(rset.getString("NAME"));
 				purchase.setRefundPoint(rset.getString("RFD_DATE"));
 				purchase.setBeneName(rset.getString("BEN_CONTENT"));
-				 
+				purchase.setPayMemo(rset.getString("PAY_MEMO"));
+				
+				int classPrice = rset.getInt("TUITION");
+				int classDays = rset.getInt("CLS_CNT");
+				int faultDays = rset.getInt("FAULT_DAYS");
+				double beneRate = rset.getInt("BEN_RATE");
+				double refundRate;
+				String oldRefundRate = rset.getString("RFD_RATE");
+				if(oldRefundRate.equals("ALL")) {
+					refundRate = 1;
+				} else if(oldRefundRate.equals("2N3")) {
+					refundRate = 2/3;
+				} else if(oldRefundRate.equals("1N2")) {
+					refundRate = 1/2;
+				} else {
+					refundRate = 0;
+				}
+				int onedayFee = classPrice / classDays;
+				int faultPrice = onedayFee * faultDays;
+				int benePrice = (int) (classPrice * beneRate);
+				int refundPrice = (int) (classPrice * refundRate);
+				int realPrice = classPrice - benePrice;
+				purchase.setClassPrice(classPrice);
+				purchase.setClassDays(classDays);
+				purchase.setFaultDays(faultDays);
+				purchase.setBeneRate(beneRate);
+				purchase.setRefundRate(refundRate);
+				purchase.setOnedayFee(onedayFee);
+				purchase.setFaultPrice(faultPrice);
+				purchase.setBenePrice(benePrice);
+				purchase.setRefundPrice(refundPrice);
+				purchase.setRealPrice(realPrice);
+				int profit;
+				if(refundPrice == 0) {
+					profit = realPrice;
+				} else {
+					if(realPrice == classPrice) {
+						profit = realPrice - refundPrice;
+					} else {
+						profit = classPrice - refundPrice;
+					}
+				}
+				
+				String detailStatus;
+				String payStatus = rset.getString("PAY_STATUS");
+				purchase.setPayStatus(payStatus);
+				if(payStatus.equals("Y")) {
+					if(refundPrice == 0) {
+						detailStatus = "완납";
+					} else {
+						detailStatus = "완납 후 환불";
+					}
+				} else {
+					detailStatus = "미납";
+				}
+				
+				purList.add(purchase);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
 		}
 		return purList;
 	}
--- src/hagong/academy/mngStudent/mngPurchase/model/service/PurchaseService.java
+++ src/hagong/academy/mngStudent/mngPurchase/model/service/PurchaseService.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import hagong.academy.mngStudent.mngPurchase.model.dao.PurchaseDao;
 import hagong.academy.mngStudent.mngPurchase.model.vo.Purchase;
+import hagong.academy.mngStudent.mngPurchase.model.vo.SelectClass;
 import hagong.academy.mngStudent.mngPurchase.model.vo.SelectDate;
 
 public class PurchaseService {
@@ -19,11 +20,19 @@ public class PurchaseService {
 		return startDay;
 	}
 
+	public ArrayList<SelectClass> selectClass(SelectDate selectDate) {
+		Connection con = getConnection();
+		ArrayList<SelectClass> selectClass = new PurchaseDao().selectClass(con, selectDate);
+		close(con);
+		return selectClass;
+	}
+
 	public ArrayList<Purchase> selectPurchaseList(SelectDate selectDate) {
 		Connection con = getConnection();
 		ArrayList<Purchase> purList = new PurchaseDao().selectPurchaseList(con, selectDate);
 		close(con);
 		return purList;
 	}
+
 
 }
