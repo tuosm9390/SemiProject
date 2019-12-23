fcc9b6ba23de1396ce30d1b77c1125ee8896b09c
Hyerin <Hyerin@DESKTOP-R382II1>
2019-12-23 18:03:44 +0900

    혜린_몰라_191223

--- src/hagong/academy/mngClass/mngAttend/controller/UpdateAttendServlet.java
+++ src/hagong/academy/mngClass/mngAttend/controller/UpdateAttendServlet.java
@@ -1,6 +1,7 @@
 package hagong.academy.mngClass.mngAttend.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import hagong.academy.mngClass.mngAttend.model.service.AttendService;
+import hagong.academy.mngStudent.mngPurchase.model.service.PurchaseService;
+import hagong.academy.mngStudent.mngPurchase.model.vo.SelectDate;
 
 @WebServlet("/aupdateAttend.attend")
 public class UpdateAttendServlet extends HttpServlet {
@@ -20,6 +23,25 @@ public class UpdateAttendServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy-MM");
+		String today = dayFormat.format(System.currentTimeMillis());
+		String[] todayArr = today.split("-");
+		String startDay = new PurchaseService().selectStartDay();
+		String[] startDayArr = startDay.split("-");
+		
+		int endYear = Integer.parseInt(todayArr[0]);
+		int endMonth = Integer.parseInt(todayArr[1]);
+		int startYear = Integer.parseInt(startDayArr[0]);
+		int startMonth = Integer.parseInt(startDayArr[1]);
+		int curYear = Integer.parseInt(request.getParameter("year"));
+		int curMonth = Integer.parseInt(request.getParameter("month"));
+		
+		SelectDate selectDate = new SelectDate();
+		selectDate.setEndYear(endYear); selectDate.setEndMonth(endMonth);
+		selectDate.setStartYear(startYear); selectDate.setStartMonth(startMonth);
+		selectDate.setSelectYear(curYear); selectDate.setSelectMonth(curMonth);
+		
 		String attStatus = request.getParameter("selectAttend");
 		String checkedPersonArr = request.getParameter("checkedPersonString");
 		String classNum = request.getParameter("classNum");
@@ -34,7 +56,8 @@ public class UpdateAttendServlet extends HttpServlet {
 		String page = "";
 		if(result > 0) {
 			page = "alistStudent.attend";
-			request.setAttribute("classNum", classNum);
+			request.setAttribute("year", curYear);
+			request.setAttribute("month", curMonth);
 			request.getRequestDispatcher(page).forward(request, response);
 		}else {
 			System.out.println("출결정보 업데이트 실패");
